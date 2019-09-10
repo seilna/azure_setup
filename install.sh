efffa435545a32e3a15bbf47b0023d57a8b7461c
seilna <seil.na@vision.snu.ac.kr>
2019-09-10 22:53:55 +0900

    .

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-r
 sudo dpkg -i cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64
 sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
 sudo apt-get updatesudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
-sudo apt-get install cuda
+sudo apt-get install -y cuda
 sudo dpkg -i libcudnn7_7.6.3.30-1+cuda10.0_amd64.deb
 
 # docker install
