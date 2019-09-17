d34c8509eaa7ba131ad981ca820e451192576c7e
Seil Na <seil.na@lunit.io>
2019-09-17 21:00:16 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # cuda install
 wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64
 wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/nvidia-machine-learning-repo-ubuntu1604_1.0.0-1_amd64.deb
+wget https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/libcudnn7_7.6.3.30-1+cuda10.0_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64
 sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
 sudo dpkg -i libcudnn7_7.6.3.30-1+cuda10.0_amd64.deb
